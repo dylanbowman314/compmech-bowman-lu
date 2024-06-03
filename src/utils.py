3cf3cc87e9bef4b530f0d0f321d57ca528d598c9
Colin Lu <1colin.lu@gmail.com>
2024-06-03 15:04:39 +0000

    Add cached beliefs and r2 file + small qol changes

--- src/utils.py
+++ src/utils.py
@@ -2,17 +2,18 @@ from pathlib import Path
 
 
 def get_cached_belief_filename(x: float, a: float):
-    return f"src/cached_belief_store/paths_beliefs_x{str(x).replace(".","")}_a{str(a).replace(".","")}.pt"
+    return f'src/cached_belief_store/paths_beliefs_x{str(x).replace(".","")}_a{str(a).replace(".","")}.pt'
 
 
 def get_jpg_filename(model_path: str, x2: float, a2: float):
-    return f"src/images/{str(model_path).split("/")[-2]}_x{str(x2).replace(".","")}_a{str(a2).replace(".","")}.jpg"
+    return f'src/images/{str(model_path).split("/")[-2]}_x{str(x2).replace(".","")}_a{str(a2).replace(".","")}.jpg'
 
 
 # TODO: these should really be env vars but oh well
 MODEL_PATH_015_06 = Path(
-    "/workspaces/cure/compmech-models/models/f6gnm1we-mess3-0.15-0.6/"
+    "/root/compmech-bowman-lu/src/models/f6gnm1we-mess3-0.15-0.6/"
 )
 MODEL_PATH_005_085 = Path(
-    "/workspaces/cure/compmech-models/models/54qc0vyb_mess3-0.05-0.85"
+    "/root/compmech-bowman-lu/src/models/54qc0vyb_mess3-0.05-0.85"
 )
+
