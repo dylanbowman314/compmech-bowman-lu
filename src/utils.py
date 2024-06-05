f2f9b3a9fda80e89473713e66f11da46e6bb5ef2
root <dylanbowman314@gmail.com>
2024-06-05 00:47:45 +0000

    changes model dir paths and some file names for beliefs

--- src/utils.py
+++ src/utils.py
@@ -10,10 +10,5 @@ def get_jpg_filename(model_path: str, x: float, a: float):
 
 
 # TODO: these should really be env vars but oh well
-MODEL_PATH_015_06 = Path(
-    "/root/compmech-bowman-lu/src/models/f6gnm1we-mess3-0.15-0.6/"
-)
-MODEL_PATH_005_085 = Path(
-    "/root/compmech-bowman-lu/src/models/54qc0vyb_mess3-0.05-0.85"
-)
-
+MODEL_PATH_015_06 = Path.cwd().resolve() / "pretrained_hmm_transformers/015_06"
+MODEL_PATH_005_085 = Path.cwd().resolve() / "pretrained_hmm_transformers/005_085"
